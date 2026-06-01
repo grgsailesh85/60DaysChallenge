d2ddf42556b797321bf1381537c75cb464b64094
Sailesh Gurung <143858404+grgsailesh85@users.noreply.github.com>
2026-06-01 22:19:43 +0545

    Add files via upload

---
+++ 60Days/Day1.py
@@ -0,0 +1,34 @@
+print("Hello World")
+
+number = 1  #Integer
+name = "Sailesh" #String
+point = 3.14 #Float
+is_true = True #Boolean
+
+print(number) #This prints the value of the variable number
+print(type(number)) #This prints the type of the variable number.
+print(name)
+print(type(name))
+print(point)
+print(type(point))
+print(is_true)
+print(type(is_true))
+
+'''
+This is used to write multiple lines of comments in the code.
+'''
+
+
+
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("the type of num1 is", type(num1))
+print("the type of num2 is", type(num2))
+
+
+sum = num1 + num2 
+
+print("The sum of the two number is", sum)
+print("the type of sum is", type(sum))
