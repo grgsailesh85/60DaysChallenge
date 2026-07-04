c6ab4e1d2acbe980a68622e662728ec0999f41b9
Sailesh Gurung <143858404+grgsailesh85@users.noreply.github.com>
2026-07-04 12:02:11 +0545

    Add files via upload

---
+++ 60Days/Day33.py
@@ -0,0 +1,7 @@
+# # Algorithm
+# 1. Load the image
+# 2. Convert into HSV format
+# 3. Define the HSV range for different range of skin color
+# 4. Create a mask using the HSV range
+# 5. Mask the original image using the created image
+# 6. Finally display the image
